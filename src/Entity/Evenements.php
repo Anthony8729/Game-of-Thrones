98dff474218172ba2cbca9a95b90d7f7310f59a2
Anthony8729 <anthony.mandon@hotmail.fr>
2019-10-02 11:09:07 +0200

    correction base de données

--- src/Entity/Evenements.php
+++ src/Entity/Evenements.php
@@ -5,9 +5,9 @@ namespace App\Entity;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
- * @ORM\Entity(repositoryClass="App\Repository\ÉvènementsRepository")
+ * @ORM\Entity(repositoryClass="App\Repository\EvenementsRepository")
  */
-class Évènements
+class Evenements
 {
     /**
      * @ORM\Id()
