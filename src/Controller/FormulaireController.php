08f507a4ca44415e9448f9647ac20545fd5d61af
Anthony8729 <anthony.mandon@hotmail.fr>
2019-10-13 18:44:34 +0200

    ajout formulaire

--- src/Controller/FormulaireController.php
+++ src/Controller/FormulaireController.php
@@ -2,7 +2,14 @@
 
 namespace App\Controller;
 
+use Doctrine\Common\Persistence\ObjectManager;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Form;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class FormulaireController extends AbstractController
@@ -10,10 +17,22 @@ class FormulaireController extends AbstractController
     /**
      * @Route("/formulaire", name="formulaire")
      */
-    public function index()
+    public function index(Request $request, ObjectManager $manager)
     {
+
+        $form = $this->createFormBuilder()
+            ->add('login', TextType::class)
+            ->add('email', EmailType::class)
+            ->add('motdepasse', PasswordType::class)
+            ->add('repetezlemotdepasse',PasswordType::class)
+            ->getForm();
+            $manager->flush();
+
+        $form->handleRequest($request);
+
         return $this->render('formulaire/index.html.twig', [
             'controller_name' => 'FormulaireController',
+            'form' => $form->createView(),
         ]);
     }
 }
